1fc925093ebdda55ecab66d39acf17dd3762f6ba
Wicin-134 <wicinskikacper42@gmail.com>
2025-10-18 19:55:04 +0200

    Update vite.config.ts

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-// https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  base: '/W/', 
+  base: mode === 'production' ? '/W/' : '/',  // 👈 FIX!
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
