407eac3808939c9f0e5a867142fff31c36b458dc
Wicin-134 <wicinskikacper42@gmail.com>
2025-10-18 20:00:44 +0200

    Update App.tsx

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Why from "./pages/Why";
-import Docs from "./pages/Docs";
+import Docs from "./pages/Drop";
 import Examples from "./pages/Examples";
 import NotFound from "./pages/NotFound";
 
@@ -16,13 +16,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/W/">
+      <BrowserRouter basename="/W/">  
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/why" element={<Why />} />
           <Route path="/docs" element={<Docs />} />
           <Route path="/examples" element={<Examples />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<App />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
