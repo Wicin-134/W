44667788647414faa0ad3d4ce9c37f33fcee483c
Wicin-134 <wicinskikacper42@gmail.com>
2025-10-18 19:36:27 +0200

    Set base URL for GitHub Pages deployment
    Added base URL configuration for GitHub Pages.

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
+  **base: '/W/'**,  // 👈 DODANE! Dla GitHub Pages (nazwa repo)
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
